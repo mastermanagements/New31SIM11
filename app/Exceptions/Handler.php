0df851ccb5e34a315096ddde3095892f9ccc0c92
Rasmin <aminrasmin@gmail.com>
2019-04-16 10:16:43 +0800

    revisi file Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,11 +47,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
 	{
+		return parent::render($request, $exception);
+		
 		//exception untuk membatasi maksimum file size yg di upload
-    if ($exception instanceof \Symfony\Component\HttpFoundation\File\Exception\FileException) {
+		if ($exception instanceof \Symfony\Component\HttpFoundation\File\Exception\FileException) {
         // create a validator and validate to throw a new ValidationException
         return Validator::make($request->all(), [
             'file_akta' => 'required|file|size:5000',
         ])->validate();
-    }
+		}
+	} 
 }
