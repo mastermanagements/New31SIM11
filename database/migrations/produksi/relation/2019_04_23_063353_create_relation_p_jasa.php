abe85adc65aef32c064369d4c4c9274a6f04fede
Fandiansyah <lastfandiansyah@gmail.com>
2019-04-23 15:02:16 +0800

    Produk -> Persiapan menu barang

---
+++ database/migrations/produksi/relation/2019_04_23_063353_create_relation_p_jasa.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRelationPJasa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('p_jasa', function (Blueprint $table) {
+            //
+            $table->foreign('id_kategori_produk')->references('id')->on('p_kategori_produk');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('p_jasa', function (Blueprint $table) {
+            //
+        });
+    }
+}
