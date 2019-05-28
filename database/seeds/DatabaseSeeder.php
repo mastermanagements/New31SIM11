127230020c61f82201523d1fe932a23da5932ab1
Rasmin <aminrasmin@gmail.com>
2019-05-28 11:24:16 +0800

    tampung hasil copyan jgn di merge

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // panggil seeder => $this->call(UsersTableSeeder::class);
+		$this->call(UserUKMTableSeeder::class);
     }
 }
