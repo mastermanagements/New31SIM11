fb74372335372b2543761f5eb8e6e13562f2e7e2
Rasmin <aminrasmin@gmail.com>
2019-06-12 10:23:15 +0800

    revisi menu perusahaan model strategi bulanan

--- app/Model/Karyawan/StrategiBulanan.php
+++ app/Model/Karyawan/StrategiBulanan.php
@@ -8,5 +8,5 @@ class StrategiBulanan extends Model
 {
    protected $table="u_strategi_bulanan";
    protected $fillable = ['id_stahunan','id_target_bulanan','isi_sbulanan','id_perusahaan','id_karyawan'];
-
+	
 }
