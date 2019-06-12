5bc56cfa850a4fc5f5464fe520aaef483d2677aa
Rasmin <aminrasmin@gmail.com>
2019-06-12 14:17:19 +0800

    tambah relasi tabel di keuangan

--- database/migrations/keuangan/relation/2019_06_12_060554_create_relation_k_rincian_pend_jasa.php
+++ database/migrations/keuangan/relation/2019_06_12_060554_create_relation_k_rincian_pend_jasa.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRelationKRencanaPendBarang extends Migration
+class CreateRelationKRincianPendJasa extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class CreateRelationKRencanaPendBarang extends Migration
      */
     public function up()
     {
-        Schema::table('k_rencana_pend_barang', function (Blueprint $table) {
-            $table->foreign('id_rab')->references('id')->on('k_rab');
+        Schema::table('k_rincian_pend_jasa', function (Blueprint $table) {
+            $table->foreign('id_rencana_pend_jasa')->references('id')->on('k_rencana_pend_jasa');
+			$table->foreign('id_jasa')->references('id')->on('p_jasa');
             $table->foreign('id_perusahaan')->references('id')->on('u_perusahaan');
             $table->foreign('id_karyawan')->references('id')->on('h_karyawan');
         });
@@ -27,8 +28,7 @@ class CreateRelationKRencanaPendBarang extends Migration
      */
     public function down()
     {
-            Schema::table('k_rencana_pend_barang', function (Blueprint $table) {
-            //
-        });
+        Schema::table('k_rincian_pend_jasa', function (Blueprint $table) {
+		});
     }
 }
