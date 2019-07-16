6dbc40f9eb3ff27772a7015b208fabd89878be09
Rasmin <aminrasmin@gmail.com>
2019-07-16 15:18:32 +0800

    tambah modal marketing

--- app/Model/Marketing/RencanaMarketingJasa.php
+++ app/Model/Marketing/RencanaMarketingJasa.php
@@ -8,15 +8,9 @@ class RencanaMarketingJasa extends Model
 {
     protected $table="m_rencana_marketing_jasa";
 
-    protected $fillable = ['id_rab','id_rencana_pend_jasa','id_rincian_pend_jasa','jum_klien_lama','jum_klien_baru','ket','id_perusahaan','id_karyawan'];
+    protected $fillable = ['id_rencana_pend_jasa','jum_klien_lama','jum_klien_baru','ket','id_perusahaan','id_karyawan'];
 
-    public function getRAB(){
-        return $this->hasOne('App\Model\Keuangan\k_rab','id_rab');
-    }
 	public function getRencanaPendJasa(){
-        return $this->hasOne('App\Model\Keuangan\k_rencana_pend_jasa','id_rencana_pend_jasa');
-    }
-	public function getRincianPendJasa(){
-        return $this->hasOne('App\Model\Keuangan\k_rincian_pend_jasa','id_rincian_pend_jasa');
+        return $this->belongsTo('App\Model\Keuangan\RencanaPendJasa','id_rencana_pend_jasa');
     }
 }
