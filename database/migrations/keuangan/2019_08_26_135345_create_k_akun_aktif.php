8d2c034c991a459aafa9ddf303a0db09b5f3683b
rasmin <aminrasmin@gmail.com>
2019-08-27 16:04:44 +0800

    tambah tabel keuangan

---
+++ database/migrations/keuangan/2019_08_26_135345_create_k_akun_aktif.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKAkunAktif extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('k_akun_aktif', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('k_akun_aktif');
+    }
+}
