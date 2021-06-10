aafc8b7d57b78be406b4b064ae4d1d3e17b9d802
GenesizVitual <lastfandiansyah@gmail.com>
2021-06-10 15:04:01 +0800

    Revisi laporan produksi, laporan pembelian, laporan penjualan

---
+++ resources/views/layouts/header_print/header_print1.blade.php
@@ -0,0 +1,34 @@
+<table style="width: 100%">
+    <thead>
+    <tr>
+        <td style="width: 100px"><img src="{{ asset('logoUsaha/'. $data['perusahaan']->logo) }}"></td>
+        <td>
+            <h2 style="text-align: center">{{ $data['perusahaan']->nm_usaha }}</h2>
+            <p style="text-align: center">
+                @if(!empty($data['perusahaan']->alamat))
+                    {{ $data['perusahaan']->alamat }},
+                @endif
+                @if(!empty($data['perusahaan']->telp))
+                    Telp: {{ $data['perusahaan']->telp }}
+                @endif
+                @if(!empty($data['perusahaan']->hp))
+                    Hp {{ $data['perusahaan']->hp }}
+                @endif
+            </p>
+            <h4 style="text-align: center">{{ $data['title'] }}</h4>
+        </td>
+        <td></td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td colspan="2">
+
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+<p style="margin-top: 50px">
+
+</p>
--- resources/views/user/manufaktur/pages/laporan/StokBarang/cetak.blade.php
+++ resources/views/user/manufaktur/pages/laporan/StokBarang/cetak.blade.php
@@ -7,10 +7,34 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Detail Stok Barang</title>
     <link rel="stylesheet" href="{{ @asset('component/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
+    <style>
+        #customers {
+            font-family: Arial, Helvetica, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
 
+        #customers td, #customers th {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+
+        #customers tr:nth-child(even){background-color: #f2f2f2;}
+
+        #customers tr:hover {background-color: #ddd;}
+
+        #customers th {
+            padding-top: 12px;
+            padding-bottom: 12px;
+            text-align: left;
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
 </head>
 <body style="margin: 20px;">
-<table class="table table-responsive table-striped">
+{!! $header !!}
+<table id="customers">
     <thead>
     <tr>
         <th>No</th>
