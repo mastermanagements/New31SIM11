aa8475709cf91070134aaea61ad3a9f30901b1c2
Rasmin <aminrasmin@gmail.com>
2021-06-14 06:37:54 -0700

    revisi tampilan kasir

--- resources/views/kasir/master.blade.php
+++ resources/views/kasir/master.blade.php
@@ -53,7 +53,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 2.4.13
         </div>
-        <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE</a>.</strong> All rights
+        <strong>Copyright &copy; 2021 <a href="https://aturusaha.com">aturusaha.com</a>.</strong> All rights
         reserved.
     </footer>
 </div>
