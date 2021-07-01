4d2bc884dab70b06977d9fc892da1f0acce9bea9
GenesizVitual <lastfandiansyah@gmail.com>
2021-07-01 15:31:19 +0800

    Manufaktur, Produksi, cetak produksi tahunan, print preview produksi tahunan

--- app/Http/Controllers/produksi/utils/Produksi.php
+++ app/Http/Controllers/produksi/utils/Produksi.php
@@ -9,6 +9,7 @@
 namespace App\Http\Controllers\produksi\utils;
 
 use App\Model\Manufaktur\P_tambah_produksi;
+use Illuminate\Support\Facades\DB;
 use Session;
 
 class Produksi
@@ -19,22 +20,43 @@ class Produksi
     public static $month = null;
     public static $barang = null;
     public static $supervisor = null;
+    public static $karyawan = null;
+
+    public static $bulan = [
+        1 => 'Januari',
+        2 => 'Februari',
+        3 => 'Maret',
+        4 => 'April',
+        5 => 'Mei',
+        6 => 'Juni',
+        7 => 'Juli',
+        8 => 'Agustus',
+        9 => 'September',
+        10 => 'Oktober',
+        11 => 'November',
+        12 => 'Desember',
+    ];
 
     public static function DataProduksi()
     {
         if (self::$year != null && self::$month != null) {
-            $produksi = P_tambah_produksi::where('status_produksi', '2')->whereMonth('tgl_mulai', self::$month)->whereYear('tgl_mulai', self::$year)->where('id_perusahaan', Session::get('id_perusahaan_karyawan'));
-        }
-        if (self::$tanggal_awal != null && self::$tanggal_akhir) {
-            $produksi = P_tambah_produksi::where('status_produksi', '2')->whereBetween('tgl_mulai', [self::$tanggal_awal, self::$tanggal_akhir])->where('id_perusahaan', Session::get('id_perusahaan_karyawan'));
+            $produksi = P_tambah_produksi::where('status_produksi', '2')->whereMonth('tgl_selesai', self::$month)->whereYear('tgl_selesai', self::$year)->where('id_perusahaan', Session::get('id_perusahaan_karyawan'));
         }
 
-        if(self::$barang !=null){
+        if (self::$tanggal_awal != null && self::$tanggal_akhir) {
+            $produksi = P_tambah_produksi::where('status_produksi', '2')->whereBetween('tgl_selesai', [self::$tanggal_awal, self::$tanggal_akhir])->where('id_perusahaan', Session::get('id_perusahaan_karyawan'));
+        }
+
+        if (self::$barang != null) {
             $produksi->where('id_barang', self::$barang);
         }
 
-        if(self::$supervisor !=null){
-            $produksi->where('id_supervisor_produksi',self::$supervisor );
+        if (self::$supervisor != null) {
+            $produksi->where('id_supervisor_produksi', self::$supervisor);
+        }
+
+        if (self::$karyawan != null) {
+            $produksi->where('id_karyawan', self::$karyawan);
         }
 
         $data = array();
@@ -44,7 +66,7 @@ class Produksi
             $column[] = $no++;
             $column[] = date('d-m-Y', strtotime($data_item->tgl_mulai));
             $column[] = date('d-m-Y', strtotime($data_item->tgl_selesai));
-            $column[] = $data_item->lama_produksi;
+            $column[] = (strtotime($data_item->tgl_mulai) - strtotime($data_item->tgl_selesai)) / 60 / 60 / 24;
             $column[] = $data_item->linkToBarang->nm_barang;
             $column[] = $data_item->jumlah_brg_jadi_bagus;
             $column[] = $data_item->jumlah_brg_jadi_rusan;
@@ -61,12 +83,79 @@ class Produksi
         return $data;
     }
 
+    public static function data_produksi_per_tahun()
+    {
+        $data = self::data_pertahun();
+        $container = array();
+        $no = 1;
+        if (!empty($data)) {
+            foreach ($data as $item) {
+                $column = array();
+                $column['bulan'] = $item->bulan;
+                $column['tgl_produksi'] = $item->tgl_mulai;
+                $column['barang'] = $item->nm_barang;
+                $column['bjb'] = $item->brg_jd_bagus;
+                $column['bjr'] = $item->brg_jd_rusak;
+                $column['bdp_b'] = $item->brg_bdp_bagus;
+                $column['bdp_r'] = $item->brg_bpd_rusak;
+                $column['supervisor'] = $item->supervisor;
+                $container[] = $column;
+            }
+        }
+        return self::_group_by($container, 'bulan');
+    }
+
+    private static function _group_by($array, $key)
+    {
+        $return = array();
+        foreach ($array as $val) {
+            $return[$val[$key]][] = $val;
+        }
+        return $return;
+    }
+
+    private static function data_pertahun()
+    {
+
+        $query_plug = '';
+
+        if (self::$year != null) {
+            $query_plug .= ' and year(p_tambah_produksi.tgl_selesai)="' . self::$year . '" ';
+        }
+
+        if (self::$karyawan != null) {
+            $query_plug .= ' and p_tambah_produksi.id_karyawan="' . self::$karyawan . '" ';
+        }
+
+        if (self::$barang != null) {
+            $query_plug .= ' and p_tambah_produksi.id_barang="' . self::$barang . '" ';
+        }
+
+        if (self::$tanggal_awal != null and self::$tanggal_akhir != null) {
+            $query_plug .= 'and p_tambah_produksi.tgl_selesai BETWEEN "' . self::$tanggal_awal . '" and "' . self::$tanggal_akhir . '"';
+        }
+
+        $query = DB::select('SELECT 
+                                    p_barang.nm_barang as nm_barang,
+                                    (p_tambah_produksi.jumlah_brg_jadi_bagus/count(p_tenaga_produksi.id)) as brg_jd_bagus,
+                                    (p_tambah_produksi.jumlah_brg_jadi_rusan/count(p_tenaga_produksi.id)) as brg_jd_rusak,
+                                    (p_tambah_produksi.jumlah_bdp_bagus/count(p_tenaga_produksi.id)) as brg_bdp_bagus,
+                                    (p_tambah_produksi.jumlah_bdp_rusak/count(p_tenaga_produksi.id)) as brg_bpd_rusak,
+                                    month(p_tambah_produksi.tgl_selesai) as bulan,
+                                    h_karyawan.nama_ky as supervisor,
+                                    p_tambah_produksi.* FROM p_tambah_produksi JOIN p_barang on p_barang.id = p_tambah_produksi.id_barang 
+                                    join p_tenaga_produksi on p_tambah_produksi.id = p_tenaga_produksi.id_tambah_produksi 
+                                    join h_karyawan on h_karyawan.id = p_tambah_produksi.id_supervisor_produksi and 
+                                    p_tambah_produksi.id_perusahaan = "' . Session::get('id_perusahaan_karyawan') . '" ' . $query_plug . ' GROUP by p_tambah_produksi.id
+                                    order by p_tambah_produksi.tgl_selesai asc');
+        return $query;
+    }
+
     private static function bahan_produksi($produksi)
     {
         $container = array();
-        if(!empty($produksi->linkToBahanProduksi))
-        {
-            foreach ($produksi->linkToBahanProduksi as $item){
+        if (!empty($produksi->linkToBahanProduksi)) {
+            foreach ($produksi->linkToBahanProduksi as $item) {
                 $container[] = $item->linkToBarang->nm_barang;
             }
         }
@@ -76,9 +165,8 @@ class Produksi
     private static function tenaga_produksi($produksi)
     {
         $container = array();
-        if(!empty($produksi->linkToMannyTenagaProduksi))
-        {
-            foreach ($produksi->linkToMannyTenagaProduksi as $item){
+        if (!empty($produksi->linkToMannyTenagaProduksi)) {
+            foreach ($produksi->linkToMannyTenagaProduksi as $item) {
                 $container[] = $item->linkToPekerja->nama_ky;
             }
         }
@@ -88,19 +176,21 @@ class Produksi
     private static function biaya_over_head($produksi)
     {
         $container = array();
-        if(!empty($produksi->linkToBiayaOverHead))
-        {
-            foreach ($produksi->linkToBiayaOverHead as $item){
+        if (!empty($produksi->linkToBiayaOverHead)) {
+            foreach ($produksi->linkToBiayaOverHead as $item) {
                 $container[] = $item->linkToOverhead->item_overhead;
             }
         }
         return $container;
     }
 
-    public static function hpp_perbarang($produski){
+    public static function hpp_perbarang($produski)
+    {
         return 0;
     }
-    public static function hpp_total($produski){
+
+    public static function hpp_total($produski)
+    {
         return 0;
     }
 }
