6e2e778c3ca8f0d640406832ff5518d40afe5692
Rasmin <aminrasmin@gmail.com>
2021-07-01 10:27:45 +0800

    revisi menu perusahaan

--- database/migrations/superadmin_sim/2019_02_26_030129_create_u_master_submenu.php
+++ database/migrations/superadmin_sim/2019_02_26_030129_create_u_master_submenu.php
@@ -16,6 +16,8 @@ class CreateUMasterSubmenu extends Migration
         Schema::create('u_master_submenu', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('id_master_menu')->unsigned();
+			$table->enum('kelompok_submenu',['0','1','2','3'])->comment('0=perdagangan, 1=jasa, 2=perdagangan dan jasa, 3=manufaktur');
+            $table->enum('jenis_submenu',['0','1'])->comment('0=menu utama, 1=menu tambahan');
             $table->string('nm_submenu');
 			$table->integer('urutan')->default('0')->nullable();
 			$table->string('url');
